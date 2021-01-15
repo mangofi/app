1d7a7f2d51be273ba54f8c6c3489f71588b66dae
Hans Gamarra <hans.gamarra1303@gmail.com>
2021-01-15 11:27:37 -0500

    using hook for connecting to wallet

--- molecules/connect-button/index.js
+++ molecules/connect-button/index.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
-import Web3 from 'web3'
 
 import connector from '../../lib/connector'
+import useWalletConnection from '../../lib/use-wallet-connection'
 
 import * as Styles from './styles'
 
@@ -13,43 +13,28 @@ function ConnectButton({ wallet, walletActions }) {
   const {
     setAccount
   } = walletActions
-
-  const loadWeb3 = async () => {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-    }
-  }
+  
+  const walletConnection = useWalletConnection()
+  useEffect(async () => {
+    await selectAccount()
+  }, [walletConnection, wallet.account])
   
   const onConnect = async () => {
-    await window.ethereum.enable()
-    await setupAccount()
+    await walletConnection.connect()
+    await selectAccount()
   }
   
-  const setupAccount = async () => {
-    if (signedIn) {
+  const selectAccount = async () => {
+    if (signedIn || !walletConnection) {
       return
     }
 
-    const web3 = window.web3
-    const accounts = await web3.eth.getAccounts()
-    
+    const accounts = await walletConnection.getAccounts()
+
     if (accounts) {
       setAccount(accounts[0])
     }
   }
-  
-  useEffect(async () => {
-    await loadWeb3()
-  }, [])
-  useEffect(async () => {
-    await setupAccount()
-  }, [wallet.account])
 
   return (
     <Styles.Container>
